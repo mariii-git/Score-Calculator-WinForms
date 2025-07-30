dffc1dae909bc387f47c0ef1ae2bcc34204abbbc
mariiiiiii <masum.s1343@gmail.com>
2025-07-29 23:34:52 -0700

    score

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace score_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnadd = new Button();
+            btnclear = new Button();
+            btncalculate = new Button();
+            lblmax = new Label();
+            lblmin = new Label();
+            lblaverage = new Label();
+            txtscore = new TextBox();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // btnadd
+            // 
+            btnadd.Location = new Point(12, 45);
+            btnadd.Name = "btnadd";
+            btnadd.Size = new Size(75, 23);
+            btnadd.TabIndex = 0;
+            btnadd.Text = "add";
+            btnadd.UseVisualStyleBackColor = true;
+            btnadd.Click += btnadd_Click;
+            // 
+            // btnclear
+            // 
+            btnclear.Location = new Point(139, 45);
+            btnclear.Name = "btnclear";
+            btnclear.Size = new Size(75, 23);
+            btnclear.TabIndex = 1;
+            btnclear.Text = "clear";
+            btnclear.UseVisualStyleBackColor = true;
+            btnclear.Click += btnclear_Click;
+            // 
+            // btncalculate
+            // 
+            btncalculate.Location = new Point(284, 45);
+            btncalculate.Name = "btncalculate";
+            btncalculate.Size = new Size(75, 23);
+            btncalculate.TabIndex = 2;
+            btncalculate.Text = "calculate";
+            btncalculate.UseVisualStyleBackColor = true;
+            btncalculate.Click += btncalculate_Click;
+            // 
+            // lblmax
+            // 
+            lblmax.AutoSize = true;
+            lblmax.Location = new Point(26, 105);
+            lblmax.Name = "lblmax";
+            lblmax.Size = new Size(38, 15);
+            lblmax.TabIndex = 3;
+            lblmax.Text = "label1";
+            // 
+            // lblmin
+            // 
+            lblmin.AutoSize = true;
+            lblmin.Location = new Point(26, 149);
+            lblmin.Name = "lblmin";
+            lblmin.Size = new Size(38, 15);
+            lblmin.TabIndex = 4;
+            lblmin.Text = "label2";
+            // 
+            // lblaverage
+            // 
+            lblaverage.AutoSize = true;
+            lblaverage.Location = new Point(26, 195);
+            lblaverage.Name = "lblaverage";
+            lblaverage.Size = new Size(38, 15);
+            lblaverage.TabIndex = 5;
+            lblaverage.Text = "label3";
+            // 
+            // txtscore
+            // 
+            txtscore.Location = new Point(124, 12);
+            txtscore.Name = "txtscore";
+            txtscore.Size = new Size(100, 23);
+            txtscore.TabIndex = 6;
+            txtscore.TextChanged += txtscore_TextChanged;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(206, 105);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(371, 222);
+            Controls.Add(listBox1);
+            Controls.Add(txtscore);
+            Controls.Add(lblaverage);
+            Controls.Add(lblmin);
+            Controls.Add(lblmax);
+            Controls.Add(btncalculate);
+            Controls.Add(btnclear);
+            Controls.Add(btnadd);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnadd;
+        private Button btnclear;
+        private Button btncalculate;
+        private Label lblmax;
+        private Label lblmin;
+        private Label lblaverage;
+        private TextBox txtscore;
+        private ListBox listBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿namespace score_calculator
+{
+    public partial class Form1 : Form
+    {
+        List<double> scores = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void addscore()
+        {
+            if (double.TryParse(txtscore.Text, out double score))
+            {
+                scores.Add(score);
+                listBox1.Items.Add(score);
+                txtscore.Clear();
+
+            }
+            else
+            {
+                MessageBox.Show("لطفا یک نمره معتبر وارد کنید.");
+            }
+        }
+        private void clearall()
+        {
+            scores.Clear();
+            listBox1.Items.Clear();
+            lblaverage.Text = "";
+            lblmax.Text = "";
+            lblmin.Text = "";
+        }
+        private void calculate()
+        {
+            if (scores.Count == 0)
+            {
+                MessageBox.Show("ابتدا نمره وارد کنید.");
+                return;
+            }
+            double average = scores.Average();
+            double max = scores.Max();
+            double min = scores.Min();
+
+            lblaverage.Text = "میانگین : " + average.ToString();
+            lblmax.Text = "بیشترین : " + max;
+            lblmin.Text = "کمترین : " + min;
+        }
+
+        private void txtscore_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            addscore();
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            clearall();
+        }
+
+        private void btncalculate_Click(object sender, EventArgs e)
+        {
+            calculate();
+        }
+    }
+}
